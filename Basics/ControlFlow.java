ded3d503ab012b3dab7eacefa8e814d7138424ac
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2026-06-18 00:16:11 +0530

    If-Else Statements

---
+++ Basics/ControlFlow.java
@@ -0,0 +1,18 @@
+package Basics;
+import java.util.Scanner;
+
+public class ControlFlow {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if(age > 18){
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+        scanner.close();
+    }
+}
