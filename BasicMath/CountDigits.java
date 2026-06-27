e90c912d126aa65166a95109cf79adea1a8d5268
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2026-06-28 01:09:21 +0530

    Add CountDigits|| method using String.valueOf

--- BasicMath/CountDigits.java
+++ BasicMath/CountDigits.java
@@ -1,7 +1,7 @@
 package BasicMath;
 
 class Solution{
-    public int CountDigits(int num){
+    public int CountDigitsI(int num){
         int count = 0;
         while(num > 0){
             num /= 10;
@@ -9,13 +9,16 @@ class Solution{
         }
         return count;
     }
+    public int CountDigitsII(int num){
+        return String.valueOf(num).length();
+    }
 }
 
 public class CountDigits {
     public static void main(String[] args){
         Solution obj = new Solution();
         int n = 54326;
-        int result = obj.CountDigits(n);
+        int result = obj.CountDigitsI(n);
 
         System.out.println("Number of Digits in " + n + " is " + result);
     }
