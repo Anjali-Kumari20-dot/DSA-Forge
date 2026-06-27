cfb7ae5d7a44b32a37834e39177a932c2ed1d545
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2026-06-27 06:35:48 +0530

    Greatest Common Divisor

---
+++ BasicMath/GreatestCommonDivisor.java
@@ -0,0 +1,47 @@
+package BasicMath;
+
+class Solution{
+
+    // Brute Force
+    public int findGcdI(int num1, int num2){
+        int gcd = 1;
+        for(int i = 2; i <= Math.min(num1, num2); i++){
+            if (num1 % i == 0 && num2 % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    // Better Approach
+    public int findGcdII(int num1, int num2){
+        int gcd = 1;
+        for(int i = Math.min(num1, num2); i > 1; i--){
+            if (num1 % i == 0 && num2 % i == 0){
+                gcd = i; break;
+            }
+        }
+        return gcd;
+    }
+
+    // Optimal Approach - Euclidean Algorithm
+    public int findGcdIII(int num1, int num2){
+        int n1 = Math.max(num1, num2);
+        int n2 = Math.min(num1, num2);
+
+        while(n1 != 0){
+            n2 = Math.min(n1, n2);
+            n1 = Math.abs(n1 - n2);
+        }
+        return n2;
+    }
+
+}
+public class GreatestCommonDivisor {
+    public static void main(String[] args){
+        Solution obj = new Solution();
+        // int gcd = obj.findGcdI(9, 12);
+        int gcd = obj.findGcdIII(9, 12);
+        System.out.println(gcd);
+    }
+}
